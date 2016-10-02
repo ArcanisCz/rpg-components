e81d85bd0755116eb6ded1dee1f200ad16dd66af
tomas.jilek <tomas.jilek@morosystems.cz>
2016-10-02 12:17:42 +0200

    webpack

--- src/IndexComponent.js
+++ src/IndexComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import main from "./main";
-import {browserHistory } from "react-router";
+import {browserHistory} from "react-router";
 
-const IndexComponent = () => <main.AppRouter history={browserHistory } />;
+const IndexComponent = () => <main.AppRouter history={browserHistory} />;
 
 export default IndexComponent;
--- src/main/AppRouter.js
+++ src/main/AppRouter.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
 import {Router, Route} from "react-router";
 
 import App from "./App";
 
 const AppRouter = ({history}) => (
     <Router history={history}>
-        <Route path="/" component={App}>
-        </Route>
+        <Route path="/" component={App} />
     </Router>
 
 );
