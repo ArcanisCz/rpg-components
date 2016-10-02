facf1480e506021ed8ea27ab312b761b67fa3834
tomas.jilek <tomas.jilek@morosystems.cz>
2016-10-02 14:09:20 +0200

    path

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import SampleComponentInternal from "./SampleComponent";
+
+export default {
+    SampleComponent: SampleComponentInternal,
+};
+
+export const SampleComponent = SampleComponentInternal;
