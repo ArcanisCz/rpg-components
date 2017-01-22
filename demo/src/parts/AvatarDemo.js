ee24c193fb0c6aa79238274075abf8ae7f5e0560
ArcanisCz <arcaniscz@gmail.com>
2017-01-23 00:07:49 +0100

    avatar

--- demo/src/parts/AvatarDemo.js
+++ demo/src/parts/AvatarDemo.js
@@ -1,16 +1,60 @@
-import React, {PropTypes} from "react";
-import {Panel, Col} from 'react-bootstrap';
+import React, {Component} from "react";
+import {Panel, Col, FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
 import {Avatar} from '../../../src';
 
-const AvatarDemo = () => (
-    <Panel header="Avatar">
-        <Col md={6}>
-            <Avatar width={300} height={300} />
-        </Col>
-        <Col md={6}>
+import avatarImg from "./img/avatar.jpg";
 
-        </Col>
-    </Panel>
-);
+export default class AvatarDemo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: 200,
+            height: 200,
+        };
+        this.changeWidth = this.changeWidth.bind(this);
+        this.changeHeight = this.changeHeight.bind(this);
+    }
 
-export default AvatarDemo;
+    changeWidth(input) {
+        this.setState({
+            width: parseInt(input.target.value || 0),
+        });
+    }
+
+    changeHeight(input) {
+        this.setState({
+            height: parseInt(input.target.value || 0),
+        });
+    }
+
+    render() {
+        return (
+            <Panel header="Avatar">
+                <Col md={6}>
+                    <Avatar width={this.state.width} height={this.state.height} img={avatarImg} />
+                </Col>
+                <Col md={6}>
+                    <FormGroup controlId="formControlsText">
+                        <ControlLabel>Width</ControlLabel>
+                        <FormControl
+                            id="formControlsText"
+                            type="number"
+                            value={this.state.width}
+                            onChange={this.changeWidth}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="formControlsText1">
+                        <ControlLabel>Height</ControlLabel>
+                        <FormControl
+                            id="formControlsText1"
+                            type="number"
+                            value={this.state.height}
+                            onChange={this.changeHeight}
+                        />
+                    </FormGroup>
+                </Col>
+            </Panel>
+        );
+    }
+}
+
--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,14 +1,17 @@
 import React, {PropTypes} from "react";
 
 const Avatar = ({width, height, img}) => (
-    <div className="rpg-components-avatar" style={{width, height}}>
-
+    <div
+        className="rpg-components-avatar"
+        style={{width, height, "background-image": "url("+img+")"}}
+    >
     </div>
 );
 
 Avatar.propTypes = {
     width: PropTypes.number.isRequired,
     height: PropTypes.number.isRequired,
+    img: PropTypes.string.isRequired,
 };
 
 export default Avatar;
